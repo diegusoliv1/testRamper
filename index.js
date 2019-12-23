a9438795fb41785b85706a79e533ef4ddc8d2bbb
Diego Oliveira <cosin.diego@voeazul.com.br>
2019-12-23 06:35:08 -0200

    ajuste package

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const rp = require('request-promise')
 const cheerio = require('cheerio')
 const { getPhone, getWebsite } = require('./getters')
 
-//Alterar aqui a URL de busca;
-const url = 'https://www.facebook.com/pg/rampersoftware/about/'
+const urlBusca = 'https://www.facebook.com/pg/rampersoftware/about/';
 
 const options = {
-  uri: url,
+  uri: urlBusca,
   headers: {
     'User-Agent': 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
   },
@@ -16,6 +15,7 @@ const options = {
 
 const request = rp(options)
   .then($ => {
+    console.log("Buscando...");
     const phone = getPhone($)
     const site = getWebsite($)
     console.log({ phone, site })
